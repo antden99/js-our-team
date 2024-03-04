5560b9c13c4f892722228e8bc0c79ec4bd1f8401
antden99 <antoniodenigris@icloud.com>
2024-03-04 12:41:26 +0100

    first commit
    add structure

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+console.log("tutto ok");
+
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+
+  //MILESTON 1
+
+  for (let i = 0; i < team.length; i++) {
+    const memberTeam = team[i];
+        for (const key in memberTeam) {
+            console.log(memberTeam[key]);
+        }
+  }
+
+  //MILESTONE 2
+
+  const containerEl = document.querySelector(".container");
+  //stampo in console per controllare di aver stampato bene
+  console.log(containerEl);
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+##steps
+
+- con un ciclo for ed un ciclo for-in annidato, stampo in console tutti gli oggetti contenuti nell'array;
+
+- mi seleziono un elemento della dom chiamato container
+    - all'elemento della dom selezionato inserisco il membro del team
